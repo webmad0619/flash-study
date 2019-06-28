2118d34309765516eff3808574b633b96f4e8ee4
Dani Vicario <danigv.email@gmail.com>
2019-06-28 12:28:17 +0200

    signup process implemented

---
+++ authorization/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+
+/* GET home page */
+router.get("/signup", (req, res) => {
+  res.render("signup")
+})
+
+router.post("/signup-user", (req, res) => {
+  if (req.body.password.length < 8) {
+    res.render('signup', {errorMessage: "weak password"});
+    return
+  }
+
+  User.
+    findOne({username: req.body.username})
+    .then(foundUser => {
+      if (foundUser) {
+        res.render('signup', {errorMessage: "user already exists! :)"});
+        return
+      } else {
+        const saltRounds = 1
+        const theSalt = bcrypt.genSaltSync(saltRounds)
+        const hashedPassword = bcrypt.hashSync(req.body.password, theSalt)
+      
+        User
+          .create({ username: req.body.username, password: hashedPassword })
+          .then(userCreatedData => {
+            res.render('index', userCreatedData);
+          })
+      }
+    })
+
+ 
+})
+
+module.exports = router;
